fa0b3cf9b1cb426e5b6066328b8f4501b2b892a5
r742davis <richard.trow.davis@gmail.com>
2024-05-17 11:27:41 -0400

    Notes model, GET and GET :id routes

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const Note = require("./models/Note");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log("Could not connect to MongoDB", err));
 
-app.get("/", (req, res) => {
-  res.send("Sanctuary works - muhahahaha!");
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+// GET ALL NOTES
+app.get("/", async (req, res) => {
+  const result = await Note.find();
+  return res.json(result);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// GET SINGLE NOTE
+app.get("/:id", async (req, res) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (!note) {
+        console.log("Note not found");
+      } else {
+        res.json(note);
+      }
+    })
+    .catch((err) => console.error("Error finding note:", err));
 });
